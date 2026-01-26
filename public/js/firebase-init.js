977e73e4f51037f998170cd3f39107c2e7c18ad5
1.9.8.2.6 (AJM) <cajm23331@gmail.com>
2026-01-26 10:13:34 -0500

    Create firebase-init.js

---
+++ public/js/firebase-init.js
@@ -0,0 +1,15 @@
+// Replace with your real Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDoXSwni65CuY1_32ZE8B1nwfQO_3VNpTw",
+  authDomain: "contract-center-llc-10.firebaseapp.com",
+  projectId: "contract-center-llc-10",
+  storageBucket: "contract-center-llc-10.firebasestorage.app",
+  messagingSenderId: "323221512767",
+  appId: "1:323221512767:web:6421260f875997dbf64e8a",
+  measurementId: "G-S2RJ0C6BWH"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
